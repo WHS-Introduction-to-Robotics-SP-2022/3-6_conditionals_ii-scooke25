cf13b3c574d2eb1032889da2d3ee7775fc63dec2
scooke25 <99276515+scooke25@users.noreply.github.com>
2022-05-19 13:34:55 -0600

    Update 3.7-Analog_Input_Control.ino

--- 3.7-Analog_Input_Control.ino
+++ 3.7-Analog_Input_Control.ino
@@ -1,35 +1,55 @@
-int red = 11;
-int ValueGreen;
-int ValueRed;
-const int redPin = 11;
-const int bluePin = 9;
-const int greenPin = 10;
-int ValueBlue;
-const int potPin = A0;
-void setup()
-{
-	pinMode(bluePin, OUTPUT);
-	pinMode(greenPin, OUTPUT);
-	pinMode(redPin, OUTPUT);
+int red=11;
+int green=10;
+int blue=9;
+int inPin =A0;
+int x;
+  
+void setup() {
+   pinMode(red ,OUTPUT);
+    pinMode(green,OUTPUT);
+    pinMode(blue,OUTPUT);
+    pinMode(inPin , INPUT);
+    Serial.begin(9600);
 }
-void loop()
-{
-  int potPinValue = map(analogRead(potPin), 0, 1024, 0, 255);	
-  if (potPinValue <= 42.5) {
-	ValueRed = 255;
-	ValueGreen = potPinValue * 6;
-	ValueBlue = 0;
-   } 
-   if ((potPinValue > 42.5) && (potPinValue <= 85)) {
-	ValueRed = 255 - (potPinValue - 43) * 6;
-	ValueGreen = 255;
-	ValueBlue = 0;
-   }
-   if ((potPinValue > 85) && (potPinValue <= 127.5)) {
-	ValueRed = 0;
-	ValueGreen = 255;
-	ValueBlue = (potPinValue - 85) * 6;
-   }
-   if ((potPinValue > 127.5) && (potPinValue <= 170)) {
-		ValueRed = 0;}
+
+void loop() {
+  Serial.println(x);
+  x=analogRead(inPin);
+  
+  if(x<=142){
+    analogWrite(red,255);
+    analogWrite(green,0);
+    analogWrite(blue,0);
+  }
+   else if(x > 142 && x < 284 ){
+    analogWrite(red,255);
+    analogWrite(green,102);
+    analogWrite(blue,0);
+  }
+   else if (x > 284 && x < 426){
+    analogWrite(red,255); 
+    analogWrite(green,255);
+    analogWrite(blue,0);
+  }
+   else if (x > 426 && x < 568){
+    analogWrite(red,0);
+    analogWrite(green,255);
+    analogWrite(blue,0);
+  }
+   else if (x > 568 && x < 710){
+    analogWrite(red,0);
+    analogWrite(green,0);
+    analogWrite(blue,255);
+  }
+   else if (x > 710 && x < 852){
+    analogWrite(red,75);
+    analogWrite(green,0);
+    analogWrite(blue,130);
+  }
+  else{
+    analogWrite(red,238);
+    analogWrite(green,51);
+    analogWrite(blue,204);
+  }
 }
+    
